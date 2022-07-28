541228c31da77be2b5617e7ae2cd939c2cad3774
inzamam.535 <aidigitaldreamers@gmail.com>
2022-07-28 15:51:46 +0500

    Coming-soon-page

---
+++ README.md
@@ -0,0 +1,4 @@
+# coming-soon
+ Coming soon page
+
+ ![Alt text](/screenshot/coming.png?raw=true "Optional Title")
---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3999;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+app.get("/terms", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/terms.html"))
+);
+app.get("/privacy-policy", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/privacy-policy.html"))
+);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/404.html"));
+});
+
+app.listen(port, () =>
+  console.log(`app listening on port http://127.0.0.1:${port}!`)
+);
